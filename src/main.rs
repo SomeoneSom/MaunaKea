2170dba7ad544c5dc36083e6013113a5c1b20e09
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2022-09-26 17:58:22 -0400

    WE GOT BASIC SIMULATION CODE LFG!

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-mod gui;
-mod globals;
 mod algorithm;
-mod simulator;
-mod level;
 mod colliders;
+mod globals;
+mod gui;
+mod level;
+mod simulator;
+
+use colored::Colorize;
 
 fn main() {
+    if cfg!(windows) {
+        unsafe {
+            winapi::um::consoleapi::SetConsoleMode(
+                kernel32::GetStdHandle(winapi::um::winbase::STD_OUTPUT_HANDLE), 0x4 | 0x1);
+        }
+    }
+    println!("{}", "MaunaKea ALPHA, by atpx8".bright_cyan());
+    println!("{}", "WARNING: This is unfinished, and likely will not fully work.".red());
     let options = eframe::NativeOptions::default();
     eframe::run_native(
-        "Waterline BETA",
+        "MaunaKea ALPHA",
         options,
         Box::new(|_cc| Box::new(gui::MaunaKea::default())),
     );
