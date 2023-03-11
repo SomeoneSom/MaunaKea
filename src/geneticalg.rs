83da0bd5805b71d09cfbd6f6895c64d9ec05340e
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-03-11 00:01:15 -0500

    OrdFloat64 moment

--- src/geneticalg.rs
+++ src/geneticalg.rs
@@ -1,4 +1,18 @@
 use genevo::{operator::prelude::*, prelude::*};
+use ordered_float::OrderedFloat;
+
+#[derive(PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Debug)]
+struct OrdFloat64(OrderedFloat<f64>);
+
+impl Fitness for OrdFloat64 {
+    fn zero() -> Self {
+        Self(OrderedFloat(0f64))
+    }
+
+    fn abs_diff(&self, other: &Self) -> Self {
+        Self(OrderedFloat((self.0  - other.0).abs()))
+    }
+}
 
 type Inputs = Vec<f32>;
 
@@ -22,20 +36,20 @@ struct FitnessCalc {
     checkpoint_count: usize,
 }
 
-impl FitnessFunction<Inputs, i32> for FitnessCalc {
-    fn fitness_of(&self, phenotype: &Inputs) -> i32 {
+impl FitnessFunction<Inputs, OrdFloat64> for FitnessCalc {
+    fn fitness_of(&self, phenotype: &Inputs) -> OrdFloat64 {
         todo!()
     }
 
-    fn average(&self, a: &[i32]) -> i32 {
-        a.iter().sum::<i32>() / a.len() as i32
+    fn average(&self, a: &[OrdFloat64]) -> OrdFloat64 {
+        OrdFloat64(OrderedFloat(a.iter().map(|f| f.0.into_inner()).sum::<f64>() / (a.len() as f64)))
     }
 
-    fn highest_possible_fitness(&self) -> i32 {
-        (self.checkpoint_count * 1000) as i32
+    fn highest_possible_fitness(&self) -> OrdFloat64 {
+        OrdFloat64(OrderedFloat((self.checkpoint_count * 1000) as f64))
     }
 
-    fn lowest_possible_fitness(&self) -> i32 {
-        i32::MIN
+    fn lowest_possible_fitness(&self) -> OrdFloat64 {
+        OrdFloat64(OrderedFloat(f64::MIN))
     }
 }
