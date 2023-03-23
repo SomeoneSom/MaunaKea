3ce365028efea3154e892eb7c34c5084b607d420
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-03-23 10:34:56 -0400

    more getting basic benches setup

--- benches/mk_benches.rs
+++ benches/mk_benches.rs
@@ -1,1 +1,9 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+pub fn collision_bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Collision Benchmarks");
+    group.bench_function("collision test - 1 step - bitvec", |b| b.iter(|| 0));
+}
+
+criterion_group!(benches, collision_bench);
+criterion_main!(benches);
