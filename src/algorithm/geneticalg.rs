e2d0cc1583273b642d10130cc7c6348702aaafb5
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2023-07-28 13:43:20 -0400

    woo inputsbuilder

--- src/algorithm/geneticalg.rs
+++ src/algorithm/geneticalg.rs
@@ -38,6 +38,16 @@ impl Genotype for InputsPop {
 
 struct InputsBuilder;
 
+impl GenomeBuilder<InputsPop> for InputsBuilder {
+    fn build_genome<R>(&self, size: usize, rng: &mut R) -> InputsPop
+        where R: Rng + Sized
+    {
+        InputsPop((0..size).map(|_| {
+            rng.gen_range(0f64..360f64)
+        }).collect(), Arc::new(Mutex::new(None)))
+    }
+}
+
 /*#[derive(Clone, Debug)]
 struct PlayerSim<'a> {
     inputs: Inputs,
